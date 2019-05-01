af51b3949943ea33364b54419612bdaf292fe8dd
Filippo011235 <fadamcewicz1@gmail.com>
2019-05-01 11:13:56 +0200

    no message

--- lab04 - RPQ, Schrage/main.cpp
+++ lab04 - RPQ, Schrage/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 #define RPQ 3
+
+struct CzasyRPQ{
+    int NrZad;
+    int Czasy[RPQ];
+};
 
 void WyswietlTabRPQ(int Tab[][RPQ], int IleZadan){
     for (int i=0; i < IleZadan; i++) {
@@ -15,35 +21,77 @@ void WyswietlTabRPQ(int Tab[][RPQ], int IleZadan){
 }
 
 void Schrage(int ZadaneDane[][RPQ], int IleZadan, int PiS[]){
-
-
-    // zmienic wektor
-
-
-    int i = 1; // numer kolejnosci zadania dodawanego
+    int i = 0; // numer kolejnosci zadania dodawanego
     int Sigma[IleZadan] = {}; // Czesciowa kolejnosc uszeregowanych zadan // Czy rozmiar dobry?
-    int Ngotowych[IleZadan][RPQ] = {}; // zbior zadan gotowych do uszeregowania
-    int Nniegotowych[IleZadan][RPQ]; // zbior zadan nieuszeregowanych
+    // int Ngotowych[IleZadan][RPQ] = {}; // zbior zadan gotowych do uszeregowania
+    // int Nniegotowych[IleZadan][RPQ]; // zbior zadan nieuszeregowanych
     int t; // zmienna pomocnicza czas
+    int IndWybranegoZadJ;        // j*
+    std::vector<CzasyRPQ> Ngotowych;
+    std::vector<CzasyRPQ> Nniegotowych;
+    CzasyRPQ Kokos;
 
-    for (int i=0; i < IleZadan; i++) {
+    for(int i = 0; i < IleZadan; i++) {
+        Kokos.NrZad = i;
         for (int j=0; j < RPQ; j++) {
-            Nniegotowych[i][j] =  ZadaneDane[i][j];
+            Kokos.Czasy[j] =  ZadaneDane[i][j];
         }
+        Nniegotowych.push_back(Kokos);
     }
+
+    // for (int i=0; i < IleZadan; i++) {
+    //     for (int j=0; j < RPQ; j++) {
+    //         Nniegotowych[i][j] =  ZadaneDane[i][j];
+    //     }
+    // }
 
     // inicjalizowanie poczatkowego czasu t
     t = 1000000000;               // skrajnie duzy czas
-    for (int i=0; i < IleZadan; i++) {
-        if(Nniegotowych[i][0] < t){     // jak remis to bierze pierwszy z najmniejszym czasem
-            t = Nniegotowych[i][0];
+    for (int i=0; i < Nniegotowych.size(); i++) {
+        if(Nniegotowych[i].Czasy[0] < t){     // jak remis to bierze pierwszy z najmniejszym czasem
+            t = Nniegotowych[i].Czasy[0];
         }
     }
-
-    while( ||)
-
-
+    
+    while( !Nniegotowych.empty() || !Ngotowych.empty()){
+    
+    cout << "Pierwszy while, ktory sie zapetla:" << endl;
+    
+        int Rbufor = 1000000;
+        for (int i=0; i < Nniegotowych.size(); i++) {
+            if(Nniegotowych[i].Czasy[0] < Rbufor){     // jak remis to bierze pierwszy z najmniejszym czasem
+                Rbufor = Nniegotowych[i].Czasy[0];
+                IndWybranegoZadJ = i;
+            }
+        }        
+        while(!Nniegotowych.empty() && Rbufor <= t){
+            // IndWybranegoZadJ ma juz indeks zadania z najmniejszym R, patrz wyzej
+            Ngotowych.push_back(Nniegotowych[IndWybranegoZadJ]);
+            Nniegotowych.erase(Nniegotowych.begin() + IndWybranegoZadJ);
+        }
+        if(Nniegotowych.empty()){
+            t = Rbufor;
+        } else {
+            // szukanie max q
+            int Qbufor = 0;
+            for (int i=0; i < Ngotowych.size(); i++) {
+                if(Ngotowych[i].Czasy[2] > Qbufor){     // jak remis to bierze pierwszy z najwiekszym czasem
+                    Qbufor = Ngotowych[i].Czasy[2];
+                    IndWybranegoZadJ = i;
+                }
+            }   
+            Sigma[i] = Ngotowych[IndWybranegoZadJ].NrZad;    // umyslna zamiana; najpierw przepisz, potem usun
+            i++;
+            t += Ngotowych[IndWybranegoZadJ].Czasy[1];
+            Ngotowych.erase(Ngotowych.begin() + IndWybranegoZadJ);
+        }
+    }   // while oba N nie sa puste
+    
+    for (int i=0; i < IleZadan; i++) {      // mozna bez Sigma u nas
+        PiS[i] = Sigma[i];
+    }   
 }   // Schrage
+
 
 int main(){
     // ofstream WynikNeh     ("WynikNeh.txt");         // zapis wyników Neh do pliku
@@ -64,6 +112,7 @@ int main(){
         cout << "Zad: " << IleZadan << endl; // dla sprawdzenia
 
         int ZadaneDane[IleZadan][RPQ];   // przechowuje czasy odczytane z pliku
+        int PiS[IleZadan] = {};
     
         //Odczyt z pliku
         for (int i=0; i < IleZadan; i++) {
@@ -72,6 +121,12 @@ int main(){
             }
         }
         WyswietlTabRPQ(ZadaneDane, IleZadan);
+        cout << "Kokos:" << endl;
+        Schrage(ZadaneDane, IleZadan, PiS);
+        cout << "Ostateczna kolejnosc:" << endl;
+        for (int i=0; i < IleZadan; i++) {
+            cout << PiS[i] << "   ";
+        }
     } // while czytania z pliku
 
     //********************************************************
