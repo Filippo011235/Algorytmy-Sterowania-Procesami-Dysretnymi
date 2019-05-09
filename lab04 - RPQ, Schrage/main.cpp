59e956f447fc2899f52e06def61cb451e86ea4fe
Filippo011235 <37882752+Filippo011235@users.noreply.github.com>
2019-05-09 14:28:06 +0200

    Add files via upload

--- lab04 - RPQ, Schrage/main.cpp
+++ lab04 - RPQ, Schrage/main.cpp
@@ -11,7 +11,15 @@ using namespace std;
 struct CzasyRPQ{
     int NrZad;
     int Czasy[RPQ];
+    void  operator=(const struct CzasyRPQ& B);
 };
+
+void CzasyRPQ::operator=(const struct CzasyRPQ& B) {
+    for (int i=0; i < RPQ; ++i) {
+        this->Czasy[i] = B.Czasy[i]; 
+    }
+    this->NrZad = B.NrZad;
+}
 
 void WyswietlTabRPQ(int Tab[][RPQ], int IleZadan){
     for (int i=0; i < IleZadan; i++) {
@@ -32,6 +40,27 @@ int MinR (std::vector<CzasyRPQ> Nniegotowych, int &IndWybranegoZadJ)
         }
     }
     return Rbufor;
+}
+
+struct CzasyRPQ MinR (std::vector<CzasyRPQ> Nniegotowych)
+{
+    struct CzasyRPQ bufor;
+    bufor.Czasy[0] = 1000000;
+    for (int i=0; i < Nniegotowych.size(); i++) {
+        if(Nniegotowych[i].Czasy[0] < bufor.Czasy[0]){     // jak remis to bierze pierwszy z najmniejszym czasem
+            bufor = Nniegotowych[i];
+        }
+    }
+    return bufor;
+}
+
+
+void UsunZwektora(vector<CzasyRPQ>& Kokos, struct CzasyRPQ DoUsuniecia) {
+    for (int i=0; i < Kokos.size(); i++) {
+        if (Kokos[i].NrZad == DoUsuniecia.NrZad) {
+            Kokos.erase(Kokos.begin() + i);
+        }
+    } 
 }
 
 void Schrage(int ZadaneDane[][RPQ], int IleZadan, int PiS[]){
@@ -68,16 +97,11 @@ void Schrage(int ZadaneDane[][RPQ], int IleZadan, int PiS[]){
     }
     
     while( !Nniegotowych.empty() || !Ngotowych.empty()){
-    getchar();
-    cout << "Rozmiar Nniegotowych: " << Nniegotowych.size() << endl;
-    cout << "Rozmiar Ngotowych: " << Ngotowych.size() << endl;
+    // getchar();
     
         
                 
         while(!Nniegotowych.empty() && MinR(Nniegotowych, IndWybranegoZadJ) <= t){
-            cout << "MinR -->    " << MinR(Nniegotowych, IndWybranegoZadJ) << endl;
-            cout << "T    -->    " << t << endl;
-           
 
             // IndWybranegoZadJ ma juz indeks zadania z najmniejszym R, patrz wyzej
             Ngotowych.push_back(Nniegotowych[IndWybranegoZadJ]);
@@ -96,7 +120,6 @@ void Schrage(int ZadaneDane[][RPQ], int IleZadan, int PiS[]){
             }   
             Sigma[iter] = Ngotowych[IndWybranegoZadJ].NrZad;    // umyslna zamiana; najpierw przepisz, potem usun
             iter++;
-            cout << "Usuwanie" << endl;
 
             t += Ngotowych[IndWybranegoZadJ].Czasy[1];
             Ngotowych.erase(Ngotowych.begin() + IndWybranegoZadJ);
@@ -107,6 +130,91 @@ void Schrage(int ZadaneDane[][RPQ], int IleZadan, int PiS[]){
         PiS[i] = Sigma[i];
     }   
 }   // Schrage
+
+int Schrage_pmtn(int ZadaneDane[][RPQ], int IleZadan, int PiS[]){
+    int CMAX = 0;
+    int iter = 0; // numer kolejnosci zadania dodawanego
+    int Sigma[IleZadan] = {}; // Czesciowa kolejnosc uszeregowanych zadan // Czy rozmiar dobry?
+    // int Ngotowych[IleZadan][RPQ] = {}; // zbior zadan gotowych do uszeregowania
+    // int Nniegotowych[IleZadan][RPQ]; // zbior zadan nieuszeregowanych
+    int t = 0;  // zmienna pomocnicza czas
+    struct CzasyRPQ IndObecnieWykZadL;  // obecnie wykonywane zadanie l
+    int q0 = 1000000; // czas dostarczenia
+    struct CzasyRPQ IndWybranegoZadJ;        // j*
+    std::vector<CzasyRPQ> Ngotowych;
+    std::vector<CzasyRPQ> Nniegotowych;
+    CzasyRPQ Kokos;
+
+    for(int i = 0; i < IleZadan; i++) {
+        Kokos.NrZad = i;
+        for (int j=0; j < RPQ; j++) {
+            Kokos.Czasy[j] =  ZadaneDane[i][j];
+        }
+        Nniegotowych.push_back(Kokos);
+    }
+
+    // for (int i=0; i < IleZadan; i++) {
+    //     for (int j=0; j < RPQ; j++) {
+    //         Nniegotowych[i][j] =  ZadaneDane[i][j];
+    //     }
+    // }
+
+    // inicjalizowanie poczatkowego czasu t
+    // t = 1000000000;               // skrajnie duzy czas
+    // for (int i=0; i < Nniegotowych.size(); i++) {
+    //     if(Nniegotowych[i].Czasy[0] < t){     // jak remis to bierze pierwszy z najmniejszym czasem
+    //         t = Nniegotowych[i].Czasy[0];
+    //     }
+    // }
+    
+    while( !Nniegotowych.empty() || !Ngotowych.empty()){
+    
+    
+    
+                
+        while(!Nniegotowych.empty() && MinR(Nniegotowych).Czasy[0] <= t){
+            IndWybranegoZadJ = MinR(Nniegotowych);
+
+            // IndWybranegoZadJ ma juz indeks zadania z najmniejszym R, patrz wyzej
+            Ngotowych.push_back(IndWybranegoZadJ);
+            // Nniegotowych.erase(Nniegotowych.begin() + IndWybranegoZadJ);
+            UsunZwektora(Nniegotowych, IndWybranegoZadJ);
+
+            if (IndWybranegoZadJ.Czasy[2] > IndObecnieWykZadL.Czasy[2]) {
+                IndObecnieWykZadL.Czasy[1] = t - IndWybranegoZadJ.Czasy[0];
+                t = IndWybranegoZadJ.Czasy[0];
+
+                if ( IndObecnieWykZadL.Czasy[1] > 0) {
+                    Ngotowych.push_back(IndObecnieWykZadL);
+                }
+            }
+        }
+        if(Ngotowych.empty()){
+            t = MinR(Nniegotowych).Czasy[0];
+        } else {
+            // szukanie max q
+            IndWybranegoZadJ.Czasy[2] = 0;
+            for (int i=0; i < Ngotowych.size(); i++) {
+                if(Ngotowych[i].Czasy[2] > IndWybranegoZadJ.Czasy[2]){     // jak remis to bierze pierwszy z najwiekszym czasem
+                    IndWybranegoZadJ = Ngotowych[i];
+                }
+            }  
+            UsunZwektora(Ngotowych, IndWybranegoZadJ);  
+            // Sigma[iter] = Ngotowych[IndWybranegoZadJ].NrZad;    // umyslna zamiana; najpierw przepisz, potem usun
+            IndObecnieWykZadL = IndWybranegoZadJ;   // l = *j
+            t += IndWybranegoZadJ.Czasy[1];
+            // Ngotowych.erase(Ngotowych.begin() + IndWybranegoZadJ);
+           
+
+            CMAX = (CMAX > t + IndWybranegoZadJ.Czasy[2]) ? CMAX : t + IndWybranegoZadJ.Czasy[2];
+        }
+    }   // while oba N nie sa puste
+    
+    // for (int i=0; i < IleZadan; i++) {      // mozna bez Sigma u nas
+    //     PiS[i] = Sigma[i];
+    // }   
+    return CMAX;
+}   // Schrage pmtn
 
 // ***********************************************************
 int Cmax(int ZadaneDane[][RPQ], int PiS[], int IleZadan) {
@@ -159,8 +267,7 @@ int main(){
                 PlikDane >> ZadaneDane[i][j];
             }
         }
-        WyswietlTabRPQ(ZadaneDane, IleZadan);
-        cout << "Kokos:" << endl;
+        //WyswietlTabRPQ(ZadaneDane, IleZadan);
         Schrage(ZadaneDane, IleZadan, PiS);
         cout << "Ostateczna kolejnosc:" << endl;
         for (int i=0; i < IleZadan; i++) {
@@ -169,6 +276,8 @@ int main(){
 
         // Cmax
         cout << endl << "Cmax ____________>  " << Cmax(ZadaneDane, PiS, IleZadan) << endl;
+
+        cout << endl << "Cmax pmtn _._._._>  " << Schrage_pmtn(ZadaneDane, IleZadan, PiS) << endl;
 
     } // while czytania z pliku
 
